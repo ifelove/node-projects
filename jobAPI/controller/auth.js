81596a80cc946e2f4b742912b3e3576a22ba02ea
Ifeoluwa <ifel1939@gmail.com>
2023-05-17 23:11:26 +0100

    handling error

--- jobAPI/controller/auth.js
+++ jobAPI/controller/auth.js
@@ -1,11 +1,11 @@
 const User=require('../model/User')
 const{StatusCodes}=require('http-status-codes')
-const {BadrequestError}=require('../error/badRequest-error')
+const {BadrequestError}=require('../error')
 
 
 const registerUser= async (req,res)=>{
-  const {name,email,password}=req.body
-  if(!name||!email||!password){throw new BadrequestError('') }
+ // const {name,email,password}=req.body
+ // if(!name||!email||!password){throw new BadrequestError(`Please provide email and password`) }
   const user= await User.create({...req.body})
     res.status(StatusCodes.CREATED).send({user})
 }

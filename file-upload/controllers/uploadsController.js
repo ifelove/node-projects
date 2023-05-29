b7d3600114f8d55cca4388ea4f25e43516be3749
Ifeoluwa <ifel1939@gmail.com>
2023-05-29 16:40:03 +0100

    file upload

--- file-upload/controllers/uploadsController.js
+++ file-upload/controllers/uploadsController.js
@@ -4,7 +4,7 @@ const { StatusCodes } = require("http-status-codes");
 const uploadProduct = async (req, res) => {
 console.log(req.files)
 const productImage=req.files.image
-const imagePath=path.join(__dirname,"/public/uploads/"+`${productImage.name}`)
+const imagePath=path.join(__dirname,'../public/uploads/'+`${productImage.name}`)
 await productImage.mv(imagePath)
 res.send('upload')
 };
